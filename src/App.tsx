6eecbb5a9b12b2230a37dcbcd9974dbd01710b2a
dlrm <sylenien@gmail.com>
2021-08-17 16:24:01 +0300

    fix stuff

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { DatePicker } from './components/DatePicker'
 const App: React.FC = () => {
   return (
     <div>
+      <div style={{ marginBottom: 100, width: 230, textAlign: 'center' }}>
+        made by Delirium#0001
+      </div>
       <DatePicker />
     </div>
   )
